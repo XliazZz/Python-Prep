face730fa17b8b8e3a5570ac1f30b63a8e208a28
unknown <martinezelias166@gmail.com>
2022-11-17 19:43:29 -0300

    Trabajo P02 resuelto

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,12 +1,89 @@
-mi_variable = 12
-print(mi_variable)
+#1
+var_num_ent = (1, 2, 3)
+print(var_num_ent);
 
-mi_variable2 = 'dario'
-print(mi_variable2)
+#2
+var2 = 8.5
+print(type(var2))
 
-mi_complejo = 5 + 7j
-print(mi_complejo)
+#3
+print(type(var_num_ent))
 
+#4
+var_nombre = 'Elias'
 
-mi_numero = input('Ingrese un valor')
-print('el valor ingresado fue', mi_numero)
+#5
+var_complex = 3 * 5j
+
+#6
+print(type(var_complex))
+
+#7
+pi = 3.1416
+
+#8
+var_true = True
+var_truee = 'True'
+print(var_true)
+print(var_truee)
+
+#9
+print(type(var_true))
+print(type(var_truee))
+
+#10
+var_suma = (3 + 5.5)
+
+#11
+var_sum_complex = (4j + 6j)
+print(var_sum_complex)
+
+#12
+var_sum_complexyreal = (3 + 7j + 2 + 3j)
+print(var_sum_complexyreal)
+
+#13
+(3 * 4)
+
+#14
+print(2 ** 8)
+
+#15
+print(27 / 4)
+
+#16
+print(27 // 4)
+
+#17
+print(27 % 4)
+
+#18
+print(4 * 6 + 3)
+
+#19
+print('Hola,' + ' como estas?')
+
+#20 
+print(2 == '2')
+
+#21
+print(2 == int('2'))
+
+#22
+# a = float(3,8) da error porque tiene una coma en vez de un punto.
+
+#23
+a = 3
+a -= 2
+print(a)
+
+#24
+print(1 << 2) 
+
+#25
+#No esta permitido porque no se puede sumar un string con un numero. Si los dos operandos son del mismo tipo el valor seria true.
+
+#26
+var11 = 'Esto se repite '
+var12 = 2
+print(var11 * var12 + str(var12) + ' veces')
